f2668741d25f2a8897361e18269c4c1740b1f789
Errold Núñez <errold222@gmail.com>
2025-03-16 10:52:36 -0600

    first commit

---
+++ src/app.http2.ts
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import http2 from 'http2'
+
+const server = http2.createSecureServer({
+    key: fs.readFileSync('./keys/server.key'),
+    cert: fs.readFileSync('./keys/server.crt'),
+},(req, res) => {
+
+    if( req.url === '/' ) {
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8')
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end( htmlFile )
+        return
+    } 
+
+    if(req.url?.endsWith('.js')) {
+        const jsFile = fs.readFileSync(`./public${req.url}`, 'utf-8')
+        res.writeHead(200, { 'Content-Type': 'application/javascript' })
+        res.end(jsFile)
+    } else if(req.url?.endsWith('.css')) {
+        const cssFile = fs.readFileSync(`./public${req.url}`, 'utf-8')
+        res.writeHead(200, { 'Content-Type': 'text/css' })
+        res.end(cssFile)
+    }
+})
+
+server.listen(8080, () => {
+    console.log('Server running on port 8080')
+})
